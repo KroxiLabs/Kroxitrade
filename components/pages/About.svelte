2acc68a2af112d279f1b257acbc851883ac3d366
JaViJeC <javijec@gmail.com>
2026-03-28 14:12:44 -0300

    Update About.svelte

--- components/pages/About.svelte
+++ components/pages/About.svelte
@@ -14,7 +14,7 @@
 
   <footer class="about-footer">
     <p class="about-footer__credit">
-      {translate($languageStore, "about.github")} <a href="https://github.com/KroxiLabs/Kroxitrade" target="_blank" rel="noreferrer"><strong>better-trading</strong></a>
+      {translate($languageStore, "about.github")} <a href="https://github.com/KroxiLabs/Kroxitrade" target="_blank" rel="noreferrer"><strong>Kroxitrade</strong></a>
     </p>
     <p class="about-footer__meta">{translate($languageStore, "about.version", { version })}</p>
   </footer>
