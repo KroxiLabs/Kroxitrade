f40812a9a6867108479e30383ea4ad314c541e64
JaViJeC <javijec@gmail.com>
2026-04-01 21:07:35 -0300

    Add role attribute to BookmarkFolder for improved accessibility

--- components/BookmarkFolder.svelte
+++ components/BookmarkFolder.svelte
@@ -303,6 +303,7 @@
   class:is-folder-dragging={isFolderDragging}
   class:is-folder-drag-over={isFolderDragOver}
   draggable="true"
+  role="group"
   on:dragstart={(e) => onFolderDragStart(e, folder.id || "")}
   on:dragenter={(e) => onFolderDragEnter(e, folder.id || "")}
   on:dragover|preventDefault
